f9e16aca89c27719353b41f7dc1986e65896acf5
greyde36 <greyde36@bk.ru>
2024-09-08 13:48:17 +0300

    08.09

---
+++ Datas.py
@@ -0,0 +1,29 @@
+print(5 + 2)
+print(5 - 2)
+print(5 / 2)
+print(5 * 2)
+print(5 % 2)                                       # % - остаток от деления
+print(5 // 2)                                      # // - целочисленное деление
+print(5 ** 2)                                      # ** - возведение в степень
+print(type(5))                                     #int - целое число
+print(type(2.5))                                   #float - дробное число (плавающая точка)
+print("Hello, my young friend!")
+print(type("Hello, my young friend!"))             #str (string) - строка
+print('"Hello, my young friend!"')
+print('Hello'+','+"my young friend!")
+print('1'+'1'+' 1')                                #concatenate
+print(True, False)
+print(type(True), type(False))                     #bool (boolean) - логический тип данных
+print(5>10, 10>5)
+print(1,2,3,4, 'hello', True)
+print(5>10)                                        #- 5 больше 10?
+print(5<10)                                        #- 5 меньше 10?
+print(5<=10)                                       #- 5 меньше или равно 10?
+print(5>=10)                                       #- 5 больше или равно 10?
+print(5==10)                                       #- 5 равно 10?
+print(5!=10)                                       #- 5 не равно 10?
+print(5!=5 and 5<10)                               # and - все части должны совпадать для True если 1 False ответ будет False
+print(5!=5 or 5<10)                                # or - если хоть 1 True ответ будет True
+print(int('5')+5)                                  # присвоить строке статус числа, также работает наоборот
+
+            #<-- комментарий
---
+++ task1.py
@@ -0,0 +1,11 @@
+                #1st program new
+print(9**0.5*5)
+                #2nd program new
+print(9.99>9.98 and 1000<=1000.1)
+                #3rd program new
+print((2*2+2)==(2*(2+2)))
+                #4th program new
+    #не совсем понял задание
+'123.456'
+float('123.456')
+print(int(123.456//30))
