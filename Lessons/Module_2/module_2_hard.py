17f4635ca25b86f178aabf0b34122e58be8d54e9
Сергей <greyde36@bk.ru>
2024-09-18 07:05:06 +0300

    17.09

---
+++ Lessons/Module_2/module_2_hard.py
@@ -0,0 +1,17 @@
+import random
+a = random.randint(3, 20)
+b = []
+c = []
+d = []
+for i in range(1, a + 1):
+    b.append(i)
+    c.append(i)
+for i in range(1, len(b)):
+    for j in range(1, len(b) + 1):
+        if a % (i + j) == 0 and a != j and i < j:
+            if j != i:
+                d.append(i)
+                d.append(j)
+            if i > (a / 2):
+                break
+print(a, '-', d)
