d49073da10c59377aaa7c662e15deb2694a92d24
Сергей <greyde36@bk.ru>
2024-09-20 19:37:58 +0300

    20.09

--- Lessons/Module_2/module_2_4.py
+++ Lessons/Module_2/module_2_4.py
@@ -1,11 +1,27 @@
+# numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+# primes = []
+# not_primes = []
+# for i in numbers[1:]:
+#     if i%2==0 and i//2!=1 or i%3==0 and i//3!=1 or i%5==0 and i//5!=1 or i%7==0 and i//7!=1:
+#         not_primes.append(i)
+#         continue
+#     else:
+#         primes.append(i)
+# print('Prime', list(set(primes)))
+# print('Not prime', list(set(not_primes)))
+##################################################################################################
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
 primes = []
 not_primes = []
 for i in numbers[1:]:
-    if i%2==0 and i//2!=1 or i%3==0 and i//3!=1 or i%5==0 and i//5!=1 or i%7==0 and i//7!=1:
-        not_primes.append(i)
-        continue
-    else:
+    is_prime=True
+    for j in range(2,i):
+        if i%j==0:
+            is_prime=False
+            break
+    if is_prime:
         primes.append(i)
-print('Prime', list(set(primes)))
-print('Not prime', list(set(not_primes)))
+    else:
+        not_primes.append(i)
+print(primes)
+print(not_primes)
