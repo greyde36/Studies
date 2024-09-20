5897ab09ce4ec1cb2d553b70ae29c774df68c206
Сергей <greyde36@bk.ru>
2024-09-20 19:32:49 +0300

    20.09

--- Lessons/Module_2/module_2_4.py
+++ Lessons/Module_2/module_2_4.py
@@ -2,7 +2,7 @@ numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
 primes = []
 not_primes = []
 for i in numbers[1:]:
-    if i%2==0 and i//2!=1 or i%3==0 and i//3!=1:
+    if i%2==0 and i//2!=1 or i%3==0 and i//3!=1 or i%5==0 and i//5!=1 or i%7==0 and i//7!=1:
         not_primes.append(i)
         continue
     else:
---
+++ Tips/Module_2/Datas2.6.py
@@ -0,0 +1,8 @@
+month=2
+match month:
+    case 1:
+        print('Январь')
+    case 2:
+        print('Февраль')
+    case 3:
+        print('Март')
