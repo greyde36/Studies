0c632ecf97d338626455bd0da4e42e6bc56fcd15
Сергей <greyde36@bk.ru>
2024-10-16 13:09:04 +0300

    16.10

--- Studies/Lessons/Module_4/module4_2/module4_2.py
+++ Studies/Lessons/Module_4/module4_2/module4_2.py
@@ -1,6 +1,9 @@
-def test_function ():
-    def inner_function ():
+def test_function():
+    def inner_function():
         print("Я в области видимости функции test_function")
+
     inner_function()
+
+
 test_function()
-#inner_function()  # ошибка
+# inner_function()  # ошибка
---
+++ Studies/Lessons/Module_4/module4_2/module4_3.py
@@ -0,0 +1,5 @@
+nums = [5, 6, 2, 1, 3, 4]
+
+
+def bubble_sort(ls):
+    for i in
