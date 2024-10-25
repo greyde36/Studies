d65e71a89b861ded6f3a409cac4c252e35e47a32
greyde36 (Work notebook) <greyde36@bk.ru>
2024-10-25 12:36:08 +0300

    23.10 Work notebook

---
+++ my_leisure/Lucky_ticket_popolam.py
@@ -0,0 +1,19 @@
+ticket=int(input('Введите номер билета: '))
+if len(str(ticket))%2!=0:
+    print('Билет не считаем')
+    quit()
+l_ticket=[]
+while ticket>0:
+    l_ticket.append(ticket%10)
+    ticket//=10
+l_ticket.reverse()
+part1=[]
+for i in range(0, len(l_ticket)//2):
+    part1.append(l_ticket[i])
+part2=[]
+for i in range(len(l_ticket)//2, len(l_ticket)):
+    part2.append(l_ticket[i])
+if sum(part1)==sum(part2):
+    print('Билет счастливый')
+else:
+    print('Билет несчастливый')
