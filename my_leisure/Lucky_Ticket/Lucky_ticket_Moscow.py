97b3cf2b38f08a4c304be75a419ef8b0b5f4ed6b
Сергей <greyde36@bk.ru>
2024-10-28 10:56:50 +0300

    28.10

--- my_leisure/Lucky_Ticket/Lucky_ticket_Moscow.py
+++ my_leisure/Lucky_Ticket/Lucky_ticket_Moscow.py
@@ -1,19 +1,23 @@
-ticket=int(input('Введите номер билета: '))
-if len(str(ticket))%2!=0:
-    print('Билет не считаем')
-    quit()
-l_ticket=[]
-while ticket>0:
-    l_ticket.append(ticket%10)
-    ticket//=10
-l_ticket.reverse()
-part1=[]
-for i in range(0, len(l_ticket)//2):
-    part1.append(l_ticket[i])
-part2=[]
-for i in range(len(l_ticket)//2, len(l_ticket)):
-    part2.append(l_ticket[i])
-if sum(part1)==sum(part2):
-    print('Билет счастливый')
-else:
-    print('Билет несчастливый')
+def Lucky_MSC(ticket):
+    if len(str(ticket)) % 2 != 0:
+        print('Билет не считаем')
+        quit()
+    l_ticket = []
+    while ticket > 0:
+        l_ticket.append(ticket % 10)
+        ticket //= 10
+    l_ticket.reverse()
+    part1 = []
+    for i in range(0, len(l_ticket) // 2):
+        part1.append(l_ticket[i])
+    part2 = []
+    for i in range(len(l_ticket) // 2, len(l_ticket)):
+        part2.append(l_ticket[i])
+    if sum(part1) == sum(part2):
+        print('Билет счастливый')
+    else:
+        print('Билет несчастливый')
+
+
+if __name__ == "__main__":
+    Lucky_MSC(12345678)
