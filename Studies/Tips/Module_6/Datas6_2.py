b6fc42d8102825ae2712ef4e8769b8c0eb9025a6
Сергей <greyde36@bk.ru>
2024-10-29 16:01:49 +0300

    29.10

--- Studies/Tips/Module_6/Datas6_2.py
+++ Studies/Tips/Module_6/Datas6_2.py
@@ -1,1 +1,15 @@
-#
+class Human:
+    head = True
+    _legs = True                                # _ защита от переопределения в дочерних классах
+    __arms = True                               # __ автоматически подставляет класс
+
+    def say_hello(self):
+        print('Здравствуйте')
+
+class Student(Human):  # Дочерний
+    def about(self):
+        print('Я студент')
+
+
+class Teacher(Human):
+    pass
