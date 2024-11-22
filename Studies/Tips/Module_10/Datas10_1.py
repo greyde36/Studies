7de1f31a269de5484c32e43aa5bb061e886bb6e8
Сергей <greyde36@bk.ru>
2024-11-23 00:57:29 +0300

    23.11

--- Studies/Tips/Module_10/Datas10_1.py
+++ Studies/Tips/Module_10/Datas10_1.py
@@ -1,1 +1,21 @@
-#
+import threading
+import time
+
+def func1():
+    for i in range(10):
+        time.sleep(0.5)
+        print(i,threading.current_thread())
+
+
+def func2(x):
+    for i in range(10):
+        time.sleep(1)
+        print(i,threading.current_thread())
+
+
+thread=threading.Thread(target=func2,args=(1,), daemon=True)
+thread.start()
+print(thread.is_alive())
+func1()
+print(threading.enumerate())
+print(threading.current_thread())
